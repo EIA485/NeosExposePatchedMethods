d9ae904633c8ce809f5a56dfd9e813f4d177b6a4
EIA485 <17285570+EIA485@users.noreply.github.com>
2021-10-20 14:12:34 -0700

    Add project files.

---
+++ ExposePatchedMethods/ExposePatchedMethods.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace ExposePatchedMethods
+{
+	public class ExposePatchedMethods : NeosMod
+	{
+		public override string Name => "ExposePatchedMethods";
+		public override string Author => "eia485";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/EIA485/NeosExposePatchedMethods/";
+		static HashSet<string> HarmonyIds = new HashSet<string>();
+		public override void OnEngineInit()
+		{
+			foreach (MethodBase method in Harmony.GetAllPatchedMethods())
+			{
+				foreach (string owner in Harmony.GetPatchInfo(method).Owners)
+				{
+						HarmonyIds.Add(owner);
+				}
+			}
+			Harmony harmony = new Harmony("net.eia485.ExposePatchedMethods");
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(Userspace), "OnAttach")]
+		class ExposePatchedMethodsPatch
+		{
+			static void Postfix(Userspace __instance)
+			{
+				Slot list = __instance.World.RootSlot.AddSlot("Loaded mods",false);
+				foreach (string harmonyId in HarmonyIds)
+				{
+					list.AddSlot(harmonyId);
+				}
+
+			}
+
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# ExposePatchedMethods
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that exposes what mods you have loaded to your userspace. it does this by creating a slot called "Loaded mods" with its children being all of the harmony ids that had been used to patch a method at the time this mod was called. mods are called in mod file name alphabetical order. if you want this mod to exposes all mods then prefix it's file name with zz.
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [ExposePatchedMethods.dll](https://github.com/eia485/NeosExposePatchedMethods/releases/latest/download/ExposePatchedMethods.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.

cc675e9bea5d9dd638582737500cc2713b41964d
EIA485 <17285570+EIA485@users.noreply.github.com>
2022-03-28 08:21:45 -0700

    format cs

--- ExposePatchedMethods.cs
+++ ExposePatchedMethods.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using NeosModLoader;
 using FrooxEngine;
 using System.Reflection;
@@ -6,8 +6,8 @@ using System.Collections.Generic;
 
 namespace ExposePatchedMethods
 {
-	public class ExposePatchedMethods : NeosMod
-	{
+    public class ExposePatchedMethods : NeosMod
+    {
         public override string Name => BuildInfo.Name;
         public override string Author => BuildInfo.Author;
         public override string Version => BuildInfo.Version;
@@ -16,54 +16,54 @@ namespace ExposePatchedMethods
         [AutoRegisterConfigKey]
         private static readonly ModConfigurationKey<bool> Key_Enable = new("enabled", "Enables this mod.", () => true);
 
-		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<bool> Key_ExposeEverywhere = new("Show Everywhare", "Enables this texts to User in WorldSpace. That makes show everywhare.", () => false);
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> Key_ExposeEverywhere = new("Show Everywhare", "Enables this texts to User in WorldSpace. That makes show everywhare.", () => false);
 
         private static ModConfiguration Config;
-		
-		static HashSet<string> HarmonyIds = new HashSet<string>();
-		public override void OnEngineInit()
-		{
-			Config = GetConfiguration();
+
+        static HashSet<string> HarmonyIds = new HashSet<string>();
+        public override void OnEngineInit()
+        {
+            Config = GetConfiguration();
             Config.Save(true);
             if (!Config.GetValue(Key_Enable)) return;
 
-			foreach (MethodBase method in Harmony.GetAllPatchedMethods())
-			{
-				foreach (string owner in Harmony.GetPatchInfo(method).Owners)
-				{
-						HarmonyIds.Add(owner);
-				}
-			}
-            Harmony harmony = new Harmony(BuildInfo.GUID);
-			harmony.PatchAll();
-			
-		}
-
-		[HarmonyPatch]
-		class PatchEveryWhare
-		{
-			[HarmonyPostfix]
-			[HarmonyPatch(typeof(Userspace), "OnAttach")]
-			static void UserspaceOnAttachPostfix(Userspace __instance)
-			{
-				GenList(__instance.World.RootSlot);
-			}
-			[HarmonyPostfix]
-			[HarmonyPatch(typeof(User), "Root", MethodType.Setter)]
-			static void UserRootSetterPostfix(User __instance)
+            foreach (MethodBase method in Harmony.GetAllPatchedMethods())
             {
-				if(Config.GetValue(Key_ExposeEverywhere)) GenList(__instance.Root.Slot);
+                foreach (string owner in Harmony.GetPatchInfo(method).Owners)
+                {
+                    HarmonyIds.Add(owner);
+                }
             }
-		}
+            Harmony harmony = new Harmony(BuildInfo.GUID);
+            harmony.PatchAll();
 
-		static void GenList(Slot slot)
-		{
-			Slot list = slot.AddSlot("Loaded mods", false);
-			foreach (string harmonyId in HarmonyIds)
-			{
-				list.AddSlot(harmonyId);
-			}
-		}
-	}
+        }
+
+        [HarmonyPatch]
+        class PatchEveryWhare
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(Userspace), "OnAttach")]
+            static void UserspaceOnAttachPostfix(Userspace __instance)
+            {
+                GenList(__instance.World.RootSlot);
+            }
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(User), "Root", MethodType.Setter)]
+            static void UserRootSetterPostfix(User __instance)
+            {
+                if (Config.GetValue(Key_ExposeEverywhere)) GenList(__instance.Root.Slot);
+            }
+        }
+
+        static void GenList(Slot slot)
+        {
+            Slot list = slot.AddSlot("Loaded mods", false);
+            foreach (string harmonyId in HarmonyIds)
+            {
+                list.AddSlot(harmonyId);
+            }
+        }
+    }
 }

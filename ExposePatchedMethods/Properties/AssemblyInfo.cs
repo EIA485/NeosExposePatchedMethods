404beeda23ddeefbb0b86d38605b954961d19b45
EIA485 <17285570+EIA485@users.noreply.github.com>
2022-03-28 08:43:44 -0700

    revert canges to AssemblyInfo.cs

--- ExposePatchedMethods/Properties/AssemblyInfo.cs
+++ ExposePatchedMethods/Properties/AssemblyInfo.cs
@@ -4,12 +4,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("ExposePatchedMethods")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExposePatchedMethods")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: AssemblyTitle(ExposePatchedMethods.BuildInfo.Name)]
-[assembly: AssemblyDescription(ExposePatchedMethods.BuildInfo.Description)]
-[assembly: AssemblyCompany("net.kazu0617")]
-[assembly: AssemblyProduct(ExposePatchedMethods.BuildInfo.GUID)]
-[assembly: AssemblyVersion(ExposePatchedMethods.BuildInfo.Version)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0322B2EF-7452-479D-BAE2-FCAB75033337")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -21,3 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
